5e44422106ea88b6a60043d1329d7adca3d6b330
Ramamas6 <clement02.dhalluin@free.fr>
2023-11-17 13:00:46 +0100

    Feat: add user localisation

--- app/src/main/java/com/example/treewonder/MainActivity.kt
+++ app/src/main/java/com/example/treewonder/MainActivity.kt
@@ -1,37 +1,101 @@
 package com.example.treewonder
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.motion.widget.Debug.getLocation
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.Console
+import java.util.Locale
+
+private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var googleMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val mapFragment = SupportMapFragment.newInstance()
-
         supportFragmentManager.beginTransaction()
             .add(R.id.a_main_lyt, mapFragment)
             .commit()
-
         mapFragment.getMapAsync(this)
+
     }
 
     override fun onMapReady(map: GoogleMap){
-        val gardanne = LatLng(
-            43.4525982, 5.4717363
-        )
-        map.addMarker(
-            MarkerOptions().position(gardanne).title("Gardanne").snippet("QG des ISMIN")
-        )
-
+        googleMap = map
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         map.moveCamera(CameraUpdateFactory.zoomTo(14f))
-        map.moveCamera(CameraUpdateFactory.newLatLng(gardanne))
+        getLocation()
+        val paris = LatLng(48.8589384,2.2646343)
+        map.addMarker(MarkerOptions().position(paris).title("Paris").snippet("Default position"))
     }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if(location != null) {
+                        val loc = LatLng(location.latitude, location.longitude)
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc))
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on location", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            2)
+    }
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String>,grantResults: IntArray) {
+        if (requestCode == 2) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLocation()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+
+
 }
