750fb50e2eb20aec76b5bec0aa70267f51eb756e
victor804 <victor.vaucel@gmail.com>
2023-12-03 00:40:29 +0100

    style: replace height, circumference entry with seekbar

--- app/src/main/java/com/example/treewonder/CreateTreeFragment.kt
+++ app/src/main/java/com/example/treewonder/CreateTreeFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -16,9 +18,9 @@ class CreateTreeFragment: Fragment() {
     private lateinit var edtName: EditText
     private lateinit var edtCommonName: EditText
     private lateinit var edtBotanicName: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtCircumference: EditText
-    private lateinit var edtDevelopmentStage: EditText
+    private lateinit var sldHeight: SeekBar
+    private lateinit var sldCircumference: SeekBar
+    private lateinit var spnDevelopmentStage: Spinner
 
     private lateinit var btnNext: Button
     override fun onCreateView(
@@ -30,9 +32,9 @@ class CreateTreeFragment: Fragment() {
         edtName = view.findViewById(R.id.f_create_tree_edt_name)
         edtCommonName = view.findViewById(R.id.f_create_tree_edt_common_name)
         edtBotanicName = view.findViewById(R.id.f_create_tree_edt_botanic_name)
-        edtHeight = view.findViewById(R.id.f_create_tree_edt_height)
-        edtCircumference = view.findViewById(R.id.f_create_tree_edt_circumference)
-        edtDevelopmentStage = view.findViewById(R.id.f_create_tree_edt_development_stage)
+        sldHeight = view.findViewById(R.id.f_create_tree_sld_height)
+        sldCircumference = view.findViewById(R.id.f_create_tree_sld_circumference)
+        spnDevelopmentStage = view.findViewById(R.id.f_create_tree_spn_development_stage)
 
         btnNext = view.findViewById(R.id.f_create_tree_btn_next)
         btnNext.setOnClickListener {
@@ -41,9 +43,9 @@ class CreateTreeFragment: Fragment() {
             bundle.putString("name", edtName.text.toString())
             bundle.putString("commonName", edtCommonName.text.toString())
             bundle.putString("botanicName", edtBotanicName.text.toString())
-            bundle.putInt("height", Integer.parseInt(edtHeight.text.toString()))
-            bundle.putInt("circumference", Integer.parseInt(edtCircumference.text.toString()))
-            bundle.putString("developmentStage", edtDevelopmentStage.text.toString())
+            bundle.putInt("height", sldHeight.progress)
+            bundle.putInt("circumference", sldCircumference.progress)
+            bundle.putString("developmentStage", spnDevelopmentStage.selectedItem.toString())
             nextFragment.arguments = bundle
             val transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
             transaction
