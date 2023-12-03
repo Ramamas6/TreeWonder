f4e1bb93c4b0bfe819743dd2fd63abcf2d2eb41b
Ramamas6 <clement02.dhalluin@free.fr>
2023-12-03 14:30:51 +0100

    feat: add clusterManager (regrouping LandMarks)

---
+++ app/src/main/java/com/example/treewonder/LandMark.kt
@@ -0,0 +1,33 @@
+package com.example.treewonder
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
+
+class LandMark(lat: Double, lng: Double, title: String, snippet: String) : ClusterItem {
+
+    private val position: LatLng
+    private val title: String
+    private val snippet: String
+
+    override fun getPosition(): LatLng {
+        return position
+    }
+
+    override fun getTitle(): String {
+        return title
+    }
+
+    override fun getSnippet(): String {
+        return snippet
+    }
+
+    fun getZIndex(): Float {
+        return 0f
+    }
+
+    init {
+        position = LatLng(lat, lng)
+        this.title = title
+        this.snippet = snippet
+    }
+}
