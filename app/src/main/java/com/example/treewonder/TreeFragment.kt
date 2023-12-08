543fb70db474e37995bb1139c60e461a881abd50
victor804 <victor.vaucel@gmail.com>
2023-12-08 23:57:13 +0100

    feat: add image in tree fragment

--- app/src/main/java/com/example/treewonder/TreeFragment.kt
+++ app/src/main/java/com/example/treewonder/TreeFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
+
 
 private const val ARG_TREE = "param1"
 
 class TreeFragment : Fragment() {
     private lateinit var tree: Tree
+
+    private lateinit var img: ImageView
 
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +29,21 @@ class TreeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_tree, container, false)
 
+
+
         view.findViewById<TextView>(R.id.f_tree_text_name).text = tree.name
         view.findViewById<TextView>(R.id.f_tree_text_commonName).text = tree.commonName
         view.findViewById<TextView>(R.id.f_tree_text_height).text = tree.height.toString()
         view.findViewById<TextView>(R.id.f_tree_text_cirumference).text = tree.circumference.toString()
 
         view.findViewById<TextView>(R.id.f_tree_text_description).text = tree.description
+
+        img = view.findViewById(R.id.f_tree_picture)
+        if (!tree.picture.isNullOrBlank()) {
+            Picasso.get().load(tree.picture).into(img)
+        } else{
+            Picasso.get().load(R.drawable.default_img_tree).into(img)
+        }
 
         // Inflate the layout for this fragment
         return view
